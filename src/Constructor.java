208ac310355d82c2079d46de2bf15d4c4a744551
unknown <dhas.ameet@gmail.com>
2015-10-12 16:00:03 +0100

    Commit

---
+++ src/Constructor.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Constructor {
+	
+	public static void main(String[]args){
+		constructortest Obj1, Obj3, Obj2;
+		Scanner in = new Scanner(System.in);  
+		System.out.println("Enter value ");
+		String str=in.nextLine();
+		Obj1 = new constructortest ();
+		Obj2 = new constructortest (Integer.parseInt(str));
+		Obj3 = new constructortest (Float.parseFloat(str));
+		
+	
+		
+		
+	}
+
+}
---
+++ src/constructortest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ameet
+ *
+ */
+public class constructortest {
+	int a;
+	double b;
+	constructortest(int a)
+{
+			this.a=a;
+			System.out.println("Constructor A"+ a);
+		}
+	constructortest(double b){
+		this.b=b;
+		System.out.println("Constructor B"+b);
+		}
+	constructortest()
+	{
+		System.out.println("in default constructore");
+		
+	}
+
+}
